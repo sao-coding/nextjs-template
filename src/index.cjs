d9fce1d82dca73ad4086a3e6c29a6652a330dd92
sao-coding <jeter.nice@gmail.com>
2025-05-28 18:04:04 +0800

    feat: 調整 console.log 示例的格式以保持一致性

--- src/index.cjs
+++ src/index.cjs
@@ -27,6 +27,6 @@ function add(a, b) {
   return a + b
 }
 
-console.log(add(1, 2)) // 3
-console.log(add('1', 2)) // "12"
-console.log(add(true, [])) // "true"
+// console.log(add(1, 2)) // 3
+// console.log(add('1', 2)) // "12"
+// console.log(add(true, [])) // "true"
